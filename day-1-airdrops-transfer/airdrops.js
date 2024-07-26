8296fdd8f688272bf68e0c5ddc445967296f4976
thesaam <samuel@wedeydo.com>
2024-07-26 03:35:04 +0100

    updates 2

--- day-1-airdrops-transfer/airdrops.js
+++ day-1-airdrops-transfer/airdrops.js
@@ -1,28 +1,28 @@
 import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js'
 
-import { generateKey } from './keygen.js';
+// import { generateKey } from './keygen.js';
 
-import walletKey from './wallet.json' assert {type: 'json'}
+import walletKey from '../wallet.json' assert {type: 'json'}
 
 
 export const AirdropCoins = async () => {
 
   const connection = new Connection('https://api.devnet.solana.com', 'confirmed');
 
-  const myAddress = Keypair.fromSecretKey(new Uint8Array(walletKey)).publicKey.toBase58();
+  const myAddress = Keypair.fromSecretKey(new Uint8Array(walletKey)).publicKey.toBase58(); // generate public key from private key
 
   console.log({myAddress})
 
-  const myAddressPublicKey = new PublicKey(myAddress);
+  const myAddressPublicKey = new PublicKey(myAddress); //verify a keypair ensuring public matches secret
 
-  const signature = await connection.requestAirdrop(myAddressPublicKey, LAMPORTS_PER_SOL * 0.4)
+  const signature = await connection.requestAirdrop(myAddressPublicKey, LAMPORTS_PER_SOL * 0.4) // receiving sol through airdrops
 
   console.log(signature);
 
-  const res = await connection.confirmTransaction(signature);
+  const res = await connection.TransactionConfirmStrategy(signature);
 
-  // console.log({res})
-  return !res.value.err
+  console.log({res})
+  // return !res.value.err
 }
 
 AirdropCoins()
